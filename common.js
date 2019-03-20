6457f468c1b17946c6f794d54c3e6e246f647728
Coray <820563721@qq.com>
2019-03-20 22:04:39 +0800

    Javascript学习

--- common.js
+++ common.js
@@ -1,3 +1,12 @@
 function my$(id) {
     return document.getElementById(id);
 }
+function addEventListener(element,type,fn) {
+    if(element.addEventListener){
+        element.addEventListener(type,fn,false);
+    }else if (element.attachEvent){
+        element.attachEvent("on"+type,fn);
+    }else {
+        element["on"+type]=fn;
+    }
+}
