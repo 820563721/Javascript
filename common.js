959c81f2b201fde948c8c938b1b13fbf8db0f2d8
Coray <820563721@qq.com>
2019-03-23 13:40:18 +0800

    Javascript学习

--- common.js
+++ common.js
@@ -1,21 +1,31 @@
 function my$(id) {
     return document.getElementById(id);
 }
-function addEventListener(element,type,fn) {
-    if(element.addEventListener){
-        element.addEventListener(type,fn,false);
-    }else if (element.attachEvent){
-        element.attachEvent("on"+type,fn);
-    }else {
-        element["on"+type]=fn;
+
+function addEventListener(element, type, fn) {
+    if (element.addEventListener) {
+        element.addEventListener(type, fn, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + type, fn);
+    } else {
+        element["on" + type] = fn;
     }
 }
-function removeEventListener(element,type,fnName) {
-    if(element.removeEventListener){
-        element.removeEventListener(type,fnName,false);
-    }else if (element.detachEvent){
-        element.detachEvent("on"+type,fnName);
-    }else {
-        element["on"+type]=null;
+
+function removeEventListener(element, type, fnName) {
+    if (element.removeEventListener) {
+        element.removeEventListener(type, fnName, false);
+    } else if (element.detachEvent) {
+        element.detachEvent("on" + type, fnName);
+    } else {
+        element["on" + type] = null;
+    }
+}
+
+function setInnerText(element, text) {
+    if (typeof element.textContent == "undefined") {
+        element.innerText = text;
+    } else {
+        element.textContent = text;
     }
 }
